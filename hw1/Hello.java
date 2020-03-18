0bba37dc3e6cf3c69d755cafff761df5b3109db3
Jonathan Cheng <jonathan950400@gmail.com>
2020-03-18 17:42:24 +0800

    Add files

---
+++ hw1/Hello.java
@@ -0,0 +1,14 @@
+package selab.myapp;
+
+/*
+*Jonathan Cheng 2020/03/18
+*/
+
+public class Hello {
+
+  public static void main(String[] args) {
+    System.out.println("Hello everyone,");
+    System.out.print("I am studying at Feng Chia University.");
+  }
+
+}
